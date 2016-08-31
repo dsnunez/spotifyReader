88599203ddd317c80d0882d1e723a3ebfd9075ec
dsnunez <dasinuza@gmail.com>
2016-08-31 13:44:02 -0300

    crear proyecto asp.net mvc + domain model con EF #2069855

---
+++ DomainModel/SpotifyContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class SpotifyContext : DbContext
+    {
+        public SpotifyContext() : base("Name=DefaultConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
