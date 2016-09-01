e380595246e7999fda7710b4443e17e57788b2f0
dsnunez <dasinuza@gmail.com>
2016-09-01 13:58:36 -0300

    agregar proyecto para la interfaz para leer la Api #2069856

---
+++ SpotifyMetadata/ApiSpotify.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel;
+
+namespace SpotifyMetadata
+{
+    public class ApiSpotify
+    {
+        internal Artist FindArtistByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SpotifyMetadata/ArtistRepository.cs
@@ -0,0 +1,20 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyMetadata
+{
+    public class ArtistRepository
+    {
+        SpotifyContext db = new SpotifyContext();
+        ApiSpotify api = new ApiSpotify();
+        public Artist GetArtistByName(string name)
+        {
+            var artist = db.Artists.FirstOrDefault(a => a.Name == name) ?? api.FindArtistByName(name);
+            return artist;
+        }
+    }
+}
