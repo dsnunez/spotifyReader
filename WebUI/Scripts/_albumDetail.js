9a8e3daf4cafe038585cbd5e5480082081b91825
dsnunez <dasinuza@gmail.com>
2016-09-08 16:37:45 -0300

    mostrar la lista de canciones cuando el usuario lo pide #2073003

---
+++ WebUI/Scripts/_albumDetail.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $(".view-tracks").click(function () {
+        $(this).text($(this).text() == "Ocultar canciones"? "Ver canciones":"Ocultar canciones");
+        var id = $(this).data("id");
+        $("#" + id).toggle();
+    });
+});
