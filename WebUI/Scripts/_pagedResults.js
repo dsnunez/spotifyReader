19c51c01be667701fa6a0d0838d5c18025b32b6e
dsnunez <dasinuza@gmail.com>
2016-09-14 20:17:41 -0300

    hacer que las filas de los resultados sean "clickeables" #2077461

--- WebUI/Scripts/_pagedResults.js
+++ WebUI/Scripts/_pagedResults.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
 
     bindClicks();
+    //markClickableLinks();
 
 });
 
--- WebUI/Scripts/search.js
+++ WebUI/Scripts/search.js
@@ -4,4 +4,14 @@
         q = encodeURIComponent(q);
         $("#searchQuery").val(q);
     });
+
+    markClickableLinks();
 });
+
+function markClickableLinks()
+{
+    $(".clickable-link").click(function () {
+        var href = $(this).data("href");
+        window.location.href = href;
+    });
+}
